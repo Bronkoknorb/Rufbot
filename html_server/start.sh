b3bc8f032189fd4ce3719c2b8da2c04503cd42a8
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2018-11-19 21:17:50 +0100

    Adapt start script

--- html_server/start.sh
+++ html_server/start.sh
@@ -6,4 +6,4 @@ BASEDIR=$(dirname "$0")
 LOGFILE="$BASEDIR/server.log"
 
 echo "Starting in background and writing log file to $LOGFILE"
-nohup python -u "$BASEDIR/manage.py" runserver 0.0.0.0:8000 "$@" &>> "$LOGFILE" &
+nohup python3 -u "$BASEDIR/main.py" "$@" &>> "$LOGFILE" &
