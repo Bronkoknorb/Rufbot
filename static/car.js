40cca55a1dc7bf52958191e1e429de028e79ff0d
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2018-12-02 18:27:27 +0100

    Enable camera moving on double tap

--- static/car.js
+++ static/car.js
@@ -1,6 +1,8 @@
-var wsProtocol = (location.protocol === "https:") ? "wss://" : "ws://";
+(function () {
 
-var path = location.pathname;
+const wsProtocol = (location.protocol === "https:") ? "wss://" : "ws://";
+
+const path = location.pathname;
 if(path.endsWith("index.html"))
 {
     path = path.substring(0, path.length - "index.html".length);
@@ -8,7 +10,7 @@ if(path.endsWith("index.html"))
 if(!path.endsWith("/")) {
     path = path + "/";
 }
-var commandWS = new WebSocket(wsProtocol + location.host + path + "command");
+const commandWS = new WebSocket(wsProtocol + location.host + path + "command");
 
 commandWS.onopen = function() {
     console.log("command connection established");
@@ -18,7 +20,38 @@ commandWS.onmessage = function(evt) {
     console.log("received command message");
 };
 
-var element = document.getElementById("gestureHandler");
+class Car {
+
+  constructor() {
+    this.locked = true;
+  }
+
+  toggle_lock() {
+    if(this.locked) {
+        this.locked = false;
+    } else {
+        this.locked = true;
+    }
+  }
+
+  video_pan_start() {
+    if(!this.locked) {
+        commandWS.send("video_pan_start");
+    }
+  }
+
+  video_pan_move(deltaX, deltaY) {
+    if(!this.locked) {
+        const command = "video_pan_move(" + deltaX + "," + deltaY +")";
+        commandWS.send(command);
+    }
+  }
+
+}
+
+const car = new Car();
+
+const element = document.getElementById("gestureHandler");
 
 const mc = new Hammer(element);
 
@@ -26,14 +59,21 @@ const mc = new Hammer(element);
 // this will block the vertical scrolling on a touch-device while on the element
 mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
+mc.get('tap').set({ taps: 2 });
+
+mc.on("tap", function(ev) {
+    car.toggle_lock();
+});
+
 mc.on("panstart", function(ev) {
-    commandWS.send("video_pan_start");
+    car.video_pan_start();
 });
 
 mc.on("panmove", function(ev) {
     // TODO don't do rounding but work with floats
     const deltaX = Math.round(ev.deltaX / 2);
     const deltaY = Math.round(ev.deltaY / 2);
-    const command = "video_pan_move(" + deltaX + "," + deltaY +")";
-    commandWS.send(command);
+    car.video_pan_move(deltaX, deltaY)
 });
+
+}());
